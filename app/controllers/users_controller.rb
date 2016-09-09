c25f7c4d50c084b2641215d9b363ed3c41c4d816
tech-camp <tech-camp@tech-camp-069-no-MacBook-Air.local>
2016-09-09 17:29:17 +0900

    add-my page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,10 @@ class UsersController < ApplicationController
   end
 
   def show
+    @username = current_user.username
+    @member = current_user.member
+    @profile = current_user.profile
+    @works = current_user.works
   end
 
 end
