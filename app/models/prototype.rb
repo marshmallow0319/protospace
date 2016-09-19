5d92211aa90fddcfd18cb2cc7fe79653f920c5aa
tech-camp <tech-camp@tech-camp-069-no-MacBook-Air.local>
2016-09-19 16:05:16 +0900

    サブ画像の有無・枚数にeditページとshowページのviewファイルを連動させる

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,6 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :photos
-  accepts_nested_attributes_for :photos
+  accepts_nested_attributes_for :photos, reject_if: proc { |attributes| attributes['content'].blank? }
   validates :title, :catch, :concept, presence: true
 end
