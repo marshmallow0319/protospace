e58af35c3604219979f4a774ce57915831adddf9
tech-camp <tech-camp@tech-camp-069-no-MacBook-Air.local>
2016-09-20 13:53:18 +0900

    n+1問題を解決

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,7 +2,7 @@ class PrototypesController < ApplicationController
   before_action :set_prototype, only: [:edit, :show, :update, :destroy]
 
   def index
-    @prototype = Prototype.order("created_at ASC").page(params[:page]).per(8)
+    @prototype = Prototype.includes(:photos).order("created_at ASC").page(params[:page]).per(12)
   end
 
   def new
