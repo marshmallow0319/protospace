f1bc731261f7804a5c02f5e8da8c902fc48d835a
tech-camp <tech-camp@tech-camp-069-no-MacBook-Air.local>
2016-09-20 19:30:22 +0900

    アクションを定義

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -4,4 +4,9 @@ class Prototype < ActiveRecord::Base
   has_many :likes, dependent: :destroy
   accepts_nested_attributes_for :photos, reject_if: proc { |attributes| attributes['content'].blank? }
   validates :title, :catch, :concept, presence: true
+
+  def like_user(user_id)
+   likes.find_by(user_id: user_id)
+  end
+  
 end
