e72bc930bb9d27f6d3eb517a1afa5e407f5c9870
tech-camp <tech-camp@tech-camp-069-no-MacBook-Air.local>
2016-09-23 19:52:49 +0900

    コメントテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160923104007) do
+ActiveRecord::Schema.define(version: 20160923104403) do
+
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.integer  "user_id",      limit: 4
+    t.integer  "prototype_id", limit: 4
+    t.text     "content",      limit: 65535
+  end
 
   create_table "likes", force: :cascade do |t|
     t.datetime "created_at",             null: false
