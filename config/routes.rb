dcd87fe0c5cc9379c2f3deb06a29198549542cbd
tech-camp <tech-camp@tech-camp-069-no-MacBook-Air.local>
2016-09-29 14:42:00 +0900

    newest機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   root 'prototypes#index'
   devise_for :users
   resources :users, only: [:edit, :show, :update]
+  namespace :prototypes do
+    resources :newest, only: :index
+  end
   resources :prototypes do
     resources :likes, only: [:create, :destroy], module: 'prototypes'
     resources :comments, only: :create, module: 'prototypes'
-    resources :newest, only: :index, module: 'prototypes'
   end
 end
