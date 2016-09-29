0b6f4e8d92fb5b30d56606a598046004672bf280
tech-camp <tech-camp@tech-camp-069-no-MacBook-Air.local>
2016-09-29 14:41:29 +0900

    comments_countが動いてなかったのを修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
-  belongs_to :prototype, counter_cache: :likes_count
+  belongs_to :prototype, counter_cache: :comments_count
   belongs_to :user
   validates :content, presence: true
 end
