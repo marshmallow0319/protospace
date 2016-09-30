aad8a4e583acc090e0e6a90ba1ed8880caf2cb2c
tech-camp <tech-camp@tech-camp-069-no-MacBook-Air.local>
2016-10-01 01:51:55 +0900

    リファクタリング

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -4,7 +4,7 @@ class Prototype < ActiveRecord::Base
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
   accepts_nested_attributes_for :photos, reject_if: proc { |attributes| attributes['content'].blank? }
-  acts_as_taggable_on :tag
+  acts_as_taggable
   validates :title, :catch, :concept, presence: true
 
   def liked_user?(user)
