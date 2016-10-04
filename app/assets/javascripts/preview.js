99b23f5f87f43d97fd1ab3817aacf04563650e57
tech-camp <marshmallow0319@gmail.com>
2016-10-04 22:45:47 +0900

    プレビュー機能実装

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -1,15 +1,19 @@
-$(document).on("change", "#js-main", function() {
-  console.log("dkny");
-  var file = this.files;
-  // 画像ファイル以外は処理を中止
-  if (!file[0].type.match(/^image\/(png|jpeg|gif)$/)) return;
-  var img    = new Image();          // 表示用のイメージオブジェクトを取得
-  var reader = new FileReader();  // ファイルリーダー作成
-  // 画像がロードされてから処理を行う
-  reader.onload = function (e) {
-      // readAsDataURLをsrcに指定してイメージを作成
-      $("#js-main").append("<img src='"+e.target.result+"' alt='canvas' width='auto' height='auto' max-width='596.66px' max-height='480px' />");
-  }
-  // readAsDataURLメソッドでファイルの内容を取得
-  reader.readAsDataURL(file[0]);
+$(document).on("turbolinks:load", function(){
+    $('.js-preview').each(function(){
+        var preview = $(this),
+        image = preview.find('.js-image');
+        image.change(
+            function(){
+            if ( !this.files.length ) {
+                return;
+            }
+            var file = this.files[0]
+            var fr = new FileReader();
+            fr.readAsDataURL(file);
+            fr.onload = function(){
+                preview.css({'background-image':'url(' + fr.result + ')', 'background-size':'contain'});
+            }
+            }
+        );
+    });
 });
